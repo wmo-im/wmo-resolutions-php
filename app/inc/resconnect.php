0b16c5e56c52c48957c9eda14a6f89e83c1534ac
maaikelimper <maaike.limper@gmail.com>
2021-12-07 15:39:56 +0100

    use super-global

--- app/inc/resconnect.php
+++ app/inc/resconnect.php
@@ -4,21 +4,26 @@
 /*connection*/
 
 
-$username = getenv('WMORESOLUTIONS_USER');
-$host     = getenv('WMORESOLUTIONS_HOST');
-$password = getenv('WMORESOLUTIONS_PWD');
-$database = getenv('WMORESOLUTIONS_DB');
+//$username = getenv('WMORESOLUTIONS_USER');
+//$host     = getenv('WMORESOLUTIONS_HOST');
+//$password = getenv('WMORESOLUTIONS_PWD');
+//$database = getenv('WMORESOLUTIONS_DB');
+
+$username = $_SERVER['WMORESOLUTIONS_USER'];
+$host     = $_SERVER['WMORESOLUTIONS_HOST'];
+$password = $_SERVER['WMORESOLUTIONS_PWD'];
+$database = $_SERVER['WMORESOLUTIONS_DB'];
 
 // Opens a connection to a MySQL server
 $mysqli = new mysqli($host,$username,$password,$database);
 
 // Check connection
 if ($mysqli -> connect_errno) {
-  echo "MYSQL-ERRNO ".$mysqli -> connect_errno;
-  echo "username: ".$username;
-  echo "host: ".$host;
-  echo "database: ".$database;
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  echo " MYSQL-ERRNO ".$mysqli -> connect_errno;
+  echo " username: ".$username;
+  echo " host: ".$host;
+  echo " database: ".$database;
+  echo " Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
 
