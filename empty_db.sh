e26e8f123737b2c9962e322ef63da16f92092ea3
maaikelimper <maaike.limper@gmail.com>
2021-12-07 14:47:44 +0100

    remove empty_db.sh

--- empty_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IFS=$'\n'
-
-for table in `mysql --user=$WMORESOLUTIONS_USER --password=$WMORESOLUTIONS_PWD --host=$WMORESOLUTIONS_HOST $WMORESOLUTIONS_DB -N -e 'SHOW TABLES'`; do
-    echo "DROP TABLE $table"
-    mysql --user=$WMORESOLUTIONS_USER  --password=$WMORESOLUTIONS_PWD --host=$WMORESOLUTIONS_HOST $WMORESOLUTIONS_DB -e "DROP TABLE $table"
-done
