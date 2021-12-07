27f3b1466d802e3368ca89f8a947a0d0cbccc0f2
maaikelimper <maaike.limper@gmail.com>
2021-12-07 16:26:46 +0100

    back to getenv

--- app/inc/resconnect.php
+++ app/inc/resconnect.php
@@ -4,15 +4,11 @@
 /*connection*/
 
 
-//$username = getenv('WMORESOLUTIONS_USER');
-//$host     = getenv('WMORESOLUTIONS_HOST');
-//$password = getenv('WMORESOLUTIONS_PWD');
-//$database = getenv('WMORESOLUTIONS_DB');
+$username = getenv('WMORESOLUTIONS_USER');
+$host     = getenv('WMORESOLUTIONS_HOST');
+$password = getenv('WMORESOLUTIONS_PWD');
+$database = getenv('WMORESOLUTIONS_DB');
 
-$username = $_ENV['WMORESOLUTIONS_USER'];
-$host     = $_ENV['WMORESOLUTIONS_HOST'];
-$password = $_ENV['WMORESOLUTIONS_PWD'];
-$database = $_ENV['WMORESOLUTIONS_DB'];
 
 // Opens a connection to a MySQL server
 $mysqli = new mysqli($host,$username,$password,$database);
@@ -20,14 +16,7 @@ $mysqli = new mysqli($host,$username,$password,$database);
 // Check connection
 if ($mysqli -> connect_errno) {
   echo " MYSQL-ERRNO ".$mysqli -> connect_errno;
-  echo " username: ".$username;
-  echo " host: ".$host;
-  echo " database: ".$database;
   echo " Failed to connect to MySQL: " . $mysqli -> connect_error;
-  echo getenv('SHELL');
-  foreach (getenv() as $key => $value) {
-    echo $key . ' - ' . $value;
-  }
   exit();
 }
 
