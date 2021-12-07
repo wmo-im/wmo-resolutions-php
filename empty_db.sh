369c57534485974f4014bf9d12517228755acaa8
maaikelimper <maaike.limper@gmail.com>
2021-12-07 13:56:40 +0100

    re-org

--- empty_db.sh
+++ empty_db.sh
@@ -2,7 +2,7 @@
 
 IFS=$'\n'
 
-for table in `mysql --user=$WMORESOLUTIONS_USER --password=$WMORESOLUTIONS_PWD --host=$WMORESOLUTIONS_HOST $WMORESOLUTIONS_DB -N -e 'SHOW TABLES'`; done
+for table in `mysql --user=$WMORESOLUTIONS_USER --password=$WMORESOLUTIONS_PWD --host=$WMORESOLUTIONS_HOST $WMORESOLUTIONS_DB -N -e 'SHOW TABLES'`; do
     echo "DROP TABLE $table"
     mysql --user=$WMORESOLUTIONS_USER  --password=$WMORESOLUTIONS_PWD --host=$WMORESOLUTIONS_HOST $WMORESOLUTIONS_DB -e "DROP TABLE $table"
 done
