62beb9c51b0d72d0f1c532042db4744821d4cb4a
Maaike <maaike.limper@gmail.com>
2021-12-21 16:46:03 +0100

    Test (#1)
    * test DB init using empty plus
    
    * re-org
    
    * test
    
    * make executable
    
    * remove empty_db.sh
    
    * add debug
    
    * use super-global
    
    * look for my env vars
    
    * back to getenv
    
    * include myphpadmin and dump of DB
    
    Co-authored-by: Ubuntu <wmo_admin@wmo-resolutions-test.ls0jvte4dcvubd2ski40ldyrzg.frax.internal.cloudapp.net>

--- app/inc/resconnect.php
+++ app/inc/resconnect.php
@@ -9,12 +9,14 @@ $host     = getenv('WMORESOLUTIONS_HOST');
 $password = getenv('WMORESOLUTIONS_PWD');
 $database = getenv('WMORESOLUTIONS_DB');
 
+
 // Opens a connection to a MySQL server
 $mysqli = new mysqli($host,$username,$password,$database);
 
 // Check connection
 if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  echo " MYSQL-ERRNO ".$mysqli -> connect_errno;
+  echo " Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
 
